6aa7d446ee73cd47d37fc8c713e5f8c599e7b44b
Luca Forziati Gangi <lucaforziati@hotmail.com>
2022-04-07 13:31:23 -0300

    se creo el template padre y los demas templates

--- Primer_entrega/Astronauta/urls.py
+++ Primer_entrega/Astronauta/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from Astronauta import views
 
 urlpatterns = [
-    
+    path("inicio/", views.prueba_template, name = "pagina_inicio"),
+    path("padre/", views.probando_padre),
+    path("iniciar/", views.iniciar, name = "pagina_iniciar"),
+    path("crear_astronauta/", views.crear_astronauta, name = "crear_astronauta"),
 ]
--- Primer_entrega/Astronauta/views.py
+++ Primer_entrega/Astronauta/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def prueba_template(request):
+
+    return render(request, "index.html")
+
+def probando_padre(request):
+
+    return render(request, "padre.html")
+
+def iniciar(request):
+
+    return render(request, "iniciar.html")
+
+def crear_astronauta(request):
+
+    return render(request, "crear_astronauta.html")
