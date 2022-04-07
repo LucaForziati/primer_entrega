4024ee34e51b3f29318dddcf467da9d7d3609979
Luca Forziati Gangi <lucaforziati@hotmail.com>
2022-04-07 16:21:30 -0300

    se agregaron los templates junto a los formularios de: marte; luna; velocidad

--- Primer_entrega/Astronauta/urls.py
+++ Primer_entrega/Astronauta/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("padre/", views.probando_padre),
     path("iniciar/", views.iniciar, name = "pagina_iniciar"),
     path("crear_astronauta/", views.crear_astronauta, name = "crear_astronauta"),
+    path("luna/", views.luna, name = "formulario_luna"),
+    path("marte/", views.marte, name = "formulario_marte"),
+    path("velocidad", views.velocidad_luz, name = "velocidad_luz")
 ]
--- Primer_entrega/Astronauta/views.py
+++ Primer_entrega/Astronauta/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
-from .models import Astronauta
-from .forms import Astronauta_formulario
+from .models import Astronauta, Peso_luna, Peso_marte, Vel_luz
+from .forms import Astronauta_formulario, Peso_luna_formulario, Peso_marte_formulario, Vel_luz_formulario
 # Create your views here.
 
 def prueba_template(request):
@@ -41,4 +41,100 @@ def crear_astronauta(request):
         astronauta_formulario = Astronauta_formulario()
 
         return render(request, "crear_astronauta.html", {"astronauta_formulario": astronauta_formulario})
+
+def luna(request):
+
+    if request.method == "POST":
+
+        luna_formulario = Peso_luna_formulario(request.POST)
+
+        if luna_formulario.is_valid():
+
+            luna_informacion = luna_formulario.cleaned_data
+            
+            luna = Peso_luna (
+                peso = luna_informacion["peso"],  
+                )
+            luna.save()
+
+            numero_astronauta = request.POST["numero_astronauta"]
+
+            peso_luna = Astronauta.objects.get(id = int(numero_astronauta)) 
+            peso_luna.peso_luna = (luna.peso / 9.81) * 1.62
+            peso_luna.save()
+
+            luna_contexto = luna_informacion
+
+            return render(request, "tu_peso_luna.html", {"luna_contexto": luna_contexto, "peso_luna": peso_luna.peso_luna})
+
+    else: 
+
+        luna_formulario = Peso_luna_formulario()
+
+        return render(request, "luna.html", {"luna_formulario": luna_formulario})
+
+def marte(request):
+
+    if request.method == "POST":
+
+        marte_formulario = Peso_marte_formulario(request.POST)
+
+        if marte_formulario.is_valid():
+
+            marte_informacion = marte_formulario.cleaned_data
+            
+            marte = Peso_marte (
+                peso = marte_informacion["peso"],  
+                )
+            marte.save()
+
+            numero_astronauta = request.POST["numero_astronauta"]
+
+            peso_marte = Astronauta.objects.get(id = int(numero_astronauta)) 
+            peso_marte.peso_marte = (marte.peso * 3.72) / 9.80
+            peso_marte.save()
+
+            marte_contexto = marte_informacion
+
+            return render(request, "tu_peso_marte.html", {"marte_contexto": marte_contexto, "peso_marte": peso_marte.peso_marte})
+
+    else: 
+
+        luna_formulario = Peso_luna_formulario()
+
+        return render(request, "luna.html", {"luna_formulario": luna_formulario})
+
+def velocidad_luz(request):
+
+    if request.method == "POST":
+
+        velocidad_formulario = Vel_luz_formulario(request.POST)
+
+        if velocidad_formulario.is_valid():
+
+            velocidad_informacion = velocidad_formulario.cleaned_data
+            
+            velocidad = Vel_luz (
+                distancia = velocidad_informacion["distancia"],  
+                )
+            velocidad.save()
+
+
+            numero_astronauta = request.POST["numero_astronauta"]
+
+            velocidad_nave = Astronauta.objects.get(id = int(numero_astronauta)) 
+            velocidad_nave.distancia_recorrida = velocidad.distancia
+            velocidad_nave.save()
+
+            velocidad_contexto = velocidad_informacion
+
+            return render(request, "velocidad_nave.html", {"velocidad_contexto": velocidad_contexto, "velocidad": velocidad_nave.distancia_recorrida})
+
+    else: 
+        velocidad_formulario = Vel_luz_formulario()
+        
+        return render(request, "velocidad.html", {"velocidad_formulario": velocidad_formulario})   
+
+
+
 
