88527e047818a0a7ff7db6a2eb0959930b82f06c
Luca Forziati Gangi <lucaforziati@hotmail.com>
2022-04-07 10:06:04 -0300

    se agregaron las clases: Astronauta; Peso_luna; Peso_marte; Vel_luz

--- Primer_entrega/Astronauta/models.py
+++ Primer_entrega/Astronauta/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class Astronauta(models.Model):
+
+    nombre = models.CharField(max_length = 30)
+    apellido = models.CharField(max_length = 30)
+    peso_luna = models.FloatField(blank = True, null = True)
+    peso_marte = models.FloatField(blank = True, null = True)
+    distancia_recorrida = models.IntegerField(blank = True, null = True)
+
+class Peso_luna(models.Model):
+
+    peso = models.FloatField()
+
+class Peso_marte(models.Model):
+
+    peso = models.FloatField()
+
+class Vel_luz(models.Model):
+
+    distancia = models.IntegerField()
