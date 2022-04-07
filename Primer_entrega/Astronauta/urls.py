66e4c50c7ba8972fd7641200585878bffeb86a48
Luca Forziati Gangi <lucaforziati@hotmail.com>
2022-04-07 17:14:59 -0300

    se agrego la funcion de buscar en el registro

--- Primer_entrega/Astronauta/urls.py
+++ Primer_entrega/Astronauta/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from Astronauta import views
 
+
 urlpatterns = [
     path("inicio/", views.prueba_template, name = "pagina_inicio"),
     path("padre/", views.probando_padre),
@@ -8,5 +9,6 @@ urlpatterns = [
     path("crear_astronauta/", views.crear_astronauta, name = "crear_astronauta"),
     path("luna/", views.luna, name = "formulario_luna"),
     path("marte/", views.marte, name = "formulario_marte"),
-    path("velocidad", views.velocidad_luz, name = "velocidad_luz")
+    path("velocidad", views.velocidad_luz, name = "velocidad_luz"),
+    path("buscar", views.buscar, name = "buscar" )
 ]
